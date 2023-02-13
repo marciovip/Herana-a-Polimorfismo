c5e4dc611371e5b04e45fa267786a337ba90aca0
marciovip <pianovip33sp@gmail.com>
2023-02-13 16:14:23 -0300

    heraca polimorfismo

---
+++ src/entites/SavingsAccount.java
@@ -0,0 +1,31 @@
+package entites;
+
+public class SavingsAccount extends Account{
+	
+	private Double interesRate;
+	
+	public SavingsAccount() {
+		super();
+	}
+
+	public SavingsAccount(Integer number, String holder, Double balance, Double interesRate) {
+		super(number, holder, balance);
+		this.interesRate = interesRate;
+	}
+
+	public Double getInteresRate() {
+		return interesRate;
+	}
+
+	public void setInteresRate(Double interesRate) {
+		this.interesRate = interesRate;
+	}
+	public void updateBalace() {
+		balance += balance * interesRate;
+	}
+	@Override
+	public void withdraw(double amount) {
+		balance -= amount;
+	}
+
+}
